23614b81d2bb3ee6eae2fda92559f2b602e87c1c
Myles Reyburn <mylesreyburn@gmail.com>
2023-02-01 22:33:23 -0700

    done

--- index.js
+++ index.js
@@ -12,5 +12,67 @@ const tutorials = [
 ];
 
 const titleCased = () => {
+
+  let tutorialCopy = tutorials.map((line) => {
+    let sentence = line.split();
+    //console.log(sentence)
+    let words = sentence[0].split(" ");
+    //console.log(words)
+    words = words.map( (individualWord) => {
+      let capitalizedWord = individualWord.charAt(0).toUpperCase() + individualWord.slice(1);
+      return capitalizedWord;
+    })
+    sentence = words.join(" ");
+  })
+
+  tutorials.map((line, index) => {
+    tutorials[index] = tutorialCopy[index]
+  })
+
+  tutorialCopy.length = 0
+  console.log(tutorials)
   return tutorials
 }
+
+titleCased();
+
+/* function testSplit(){
+  console.log(tutorials[0]);
+
+  const test = tutorials[0];
+
+  console.log(test);
+
+  const test2 = test.split();
+
+  console.log(test2)
+
+  let test3 = []; 
+
+  test2.forEach(() => {test3 = test2[0].split(" ")});
+
+  console.log(test3);
+
+  console.log(test3[0][0])
+
+  let test4 = test3.map(function (word){ 
+    console.log(word);
+    betterWord = word.charAt(0).toUpperCase() + word.slice(1);
+    console.log(betterWord)
+    return betterWord;
+  })
+
+  reconstructedSentence = test4.join(" ")
+
+  console.log(test4)
+
+  console.log(reconstructedSentence)
+
+  tutorials[0] = reconstructedSentence;
+
+  console.log(tutorials);
+}
+
+testSplit();
+
+titleCased(tutorials); */
